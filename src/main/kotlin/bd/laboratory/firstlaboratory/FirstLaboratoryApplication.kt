a8e7ae8cfef8e9fecd31eee86bf17d3ae06bed8d
Adrian Glapiński <adrian.glapinski@rsqtechnologies.com>
2019-12-10 19:12:12 +0100

    Initial commit

---
+++ src/main/kotlin/bd/laboratory/firstlaboratory/FirstLaboratoryApplication.kt
@@ -0,0 +1,11 @@
+package bd.laboratory.firstlaboratory
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FirstLaboratoryApplication
+
+fun main(args: Array<String>) {
+	runApplication<FirstLaboratoryApplication>(*args)
+}
---
+++ src/test/kotlin/bd/laboratory/firstlaboratory/FirstLaboratoryApplicationTests.kt
@@ -0,0 +1,13 @@
+package bd.laboratory.firstlaboratory
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FirstLaboratoryApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
