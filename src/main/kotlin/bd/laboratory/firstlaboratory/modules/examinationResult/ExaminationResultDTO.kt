2006e6b2b62c59f225aabe8c17592ad9dff8b9cb
Adrian Glapiński <adrian.glapinski@rsqtechnologies.com>
2019-12-12 19:26:41 +0100

    Another PR fix

--- src/main/kotlin/bd/laboratory/firstlaboratory/modules/examinationResult/ExaminationResultDTO.kt
+++ src/main/kotlin/bd/laboratory/firstlaboratory/modules/examinationResult/ExaminationResultDTO.kt
@@ -1,10 +1,11 @@
 package bd.laboratory.firstlaboratory.modules.examinationResult
 
+import bd.laboratory.firstlaboratory.modules.infrastructure.exceptions.EntityIdNullException
 import javax.persistence.EnumType
 import javax.persistence.Enumerated
 
 data class ExaminationResultDTO(
-        val id: Int? = null,
+        val id: Int,
 
         val orderNumber: String,
 
@@ -20,4 +21,26 @@ data class ExaminationResultDTO(
         val maxNormValue: Double,
 
         val isDone: Boolean = false
-)
+) {
+    constructor(examinationResult: ExaminationResult) : this(
+            id = examinationResult.id ?: throw EntityIdNullException(ExaminationResult::class),
+            orderNumber = examinationResult.orderNumber,
+            examinationType = examinationResult.examinationType,
+            unit = examinationResult.unit,
+            patientValue = examinationResult.patientValue,
+            minNormValue = examinationResult.minNormValue,
+            maxNormValue = examinationResult.maxNormValue,
+            isDone = examinationResult.isDone
+    )
+
+    fun toExaminationResultEntity() = ExaminationResult(
+            id = this.id,
+            orderNumber = this.orderNumber,
+            examinationType = this.examinationType,
+            unit = this.unit,
+            patientValue = this.patientValue,
+            minNormValue = this.minNormValue,
+            maxNormValue = this.maxNormValue,
+            isDone = this.isDone
+    )
+}
