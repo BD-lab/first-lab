506d7a54d627be3ed76d441ad5defe348d2205c8
Adrian Glapiński <adrian.glapinski@rsqtechnologies.com>
2019-12-12 19:20:09 +0100

    Another PR fix

--- src/main/kotlin/bd/laboratory/firstlaboratory/modules/order/ExaminationInfoDTO.kt
+++ src/main/kotlin/bd/laboratory/firstlaboratory/modules/order/ExaminationInfoDTO.kt
@@ -3,7 +3,7 @@ package bd.laboratory.firstlaboratory.modules.order
 import bd.laboratory.firstlaboratory.modules.examinationResult.ExaminationType
 
 class ExaminationInfoDTO(
-        val id: Int?,
+        val id: Int,
         val laboratoryId: Int,
         val type: ExaminationType,
         val isDone: Boolean
